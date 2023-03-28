67c7b244d4535d53cab7d9595230a61a6dd6218e
balabk5 <48986470+Balabk5@users.noreply.github.com>
2023-03-28 10:18:16 +0530

    added s3 bucket

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -1,54 +1,70 @@
-import puppeteer from 'puppeteer';
-import hbs from 'handlebars'
-import fs from 'fs-extra'
-import * as path from 'path';
+import puppeteer from "puppeteer";
+import hbs from "handlebars";
+import fs from "fs-extra";
+import * as path from "path";
+import AWS from "aws-sdk";
+import fetch from "node-fetch";
+import { Stream } from "stream";
+import { response } from "express";
 
-const headerImgPath = path.join(process.cwd(), "assests", "kaartechlogo.png")
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 
-const headerimgbase64 = fs.readFileSync(headerImgPath).toString('base64')
+const headerImgPath = path.join(process.cwd(), "assests", "kaartechlogo.png");
 
-const pdfController={
+const headerimgbase64 = fs.readFileSync(headerImgPath).toString("base64");
 
-    generatePdf: async function (req, res) {
-        const filename = Math.random() + "_doc" + ".pdf";
-        const PDFgeneratedFilePath = path.join(process.cwd(),".", "docs", filename);
-        
-        const tabledata = req.body.tabledata
+const pdfController = {
+  generatePdf: async function (req, res) {
+    const filename = Math.random() + "_doc" + ".pdf";
+    const PDFgeneratedFilePath = path.join(
+      process.cwd(),
+      ".",
+      "docs",
+      filename
+    );
 
-        // *spliting object key and values
-        const values = tabledata.map(doc => Object.values(doc));
-        const parameterNames = Object.keys(tabledata [0]);
+    const tabledata = req.body.tabledata;
 
-        const tableDataWithObjectName = {
-          "table":values,
-          "tableHeaders":parameterNames
-        }
+    // *spliting object key and values
+    const values = tabledata.map((doc) => Object.values(doc));
+    const parameterNames = Object.keys(tabledata[0]);
 
-        // res.status(200).send(tableDataWithObjectName.tableHeaders)
+    const tableDataWithObjectName = {
+      table: values,
+      tableHeaders: parameterNames,
+    };
 
-        // *implementing puppeteer for pdf generation
-        const compile = async function (templateName, tableData) {
-          const filePath = path.join(process.cwd(),".", "templates", `${templateName}.hbs`);
-        
-          const html = await fs.readFile(filePath, "utf8");
-          return hbs.compile(html)(tableData);
-        };
-        
-        (async function () {
-          try {
-            const browser = await puppeteer.launch();
-        
-            const page = await browser.newPage();
-        
-            const content = await compile("index", tableDataWithObjectName);
-        
-            await page.setContent(content);
-          
-        
-            await page.pdf({
-                displayHeaderFooter:true,
-              headerTemplate:
-              ` <div style="height:auto; width:100%; background-color:wheat; display: flex;  border-bottom: 1px ridge;">
+    // res.status(200).send(tableDataWithObjectName.tableHeaders)
+
+    // *implementing puppeteer for pdf generation
+    const compile = async function (templateName, tableData) {
+      const filePath = path.join(
+        process.cwd(),
+        ".",
+        "templates",
+        `${templateName}.hbs`
+      );
+
+      const html = await fs.readFile(filePath, "utf8");
+      return hbs.compile(html)(tableData);
+    };
+
+    (async function () {
+      try {
+        const browser = await puppeteer.launch();
+
+        const page = await browser.newPage();
+
+        const content = await compile("index", tableDataWithObjectName);
+
+        await page.setContent(content);
+
+        const pdf = await page.pdf({
+          displayHeaderFooter: true,
+          headerTemplate: ` <div style="height:auto; width:100%; background-color:wheat; display: flex;  border-bottom: 1px ridge;">
                   <div style="height: inherit; width: 20%; padding-left:10px;">
                     <img src="data:image/jpeg;base64,${headerimgbase64}" alt="alt text" style="height:75px; width:75px; " />
                   </div>
@@ -59,36 +75,89 @@ const pdfController={
                     <p>www.kaartech.com</p>
                   </div>
                 </div>`,
-              footerTemplate:
-                '<div style="font-size: 10px; width:100%; text-align: right; padding-right: 30px;"> <span class="pageNumber"></span></div>',
-              margin: {
-                top: "165px",
-                bottom: "125px",
-                right: "60px",
-                left: "60px",
-              },
-              path: "./docs/" + filename,
-              format: "A4",
-        
-              printBackground: true,
-            });
-            
-            console.log("done creating pdf");
-         
-            await browser.close();
-        
-            // process.exit();
-          } catch (e) {
-            console.log(e);
+          footerTemplate:
+            '<div style="font-size: 10px; width:100%; text-align: right; padding-right: 30px;"> <span class="pageNumber"></span></div>',
+          margin: {
+            top: "165px",
+            bottom: "125px",
+            right: "60px",
+            left: "60px",
+          },
+          // path: "./docs1/" + filename,
+          format: "A4",
+
+          printBackground: true,
+        });
+
+        console.log("done creating pdf");
+
+        var params = {
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Key: filename,
+          Body: pdf,
+          ContentType: "application/pdf",
+        };
+
+        // s3.putObject(params).promise()
+
+        s3.upload(params, (err, data) => {
+          if (err) {
+            console.log(err);
           }
-        })();
-        
+          console.log(data);
+          console.log(data.Location);
+          // res.download(data.Location)
 
-        console.log(parameterNames);
-        res.status(200).send(PDFgeneratedFilePath)
-       
-      },
+          params = { Bucket: process.env.AWS_BUCKET_NAME, Key: data.Key };
+          var url = s3.getSignedUrl("getObject", params);
+          console.log("The URL is", url);
+          res.status(200).send(data.Key);
+        });
 
-}
+        await browser.close();
 
-export default pdfController
+        // process.exit();
+      } catch (e) {
+        console.log(e);
+      }
+    })();
+  },
+
+  downloadPdf: async function (req, res) {
+    try {
+      const url = req.query.url;
+      console.log(url);
+      // res.download(url)
+      s3.getObject(
+        { Bucket: process.env.AWS_BUCKET_NAME, Key: url },
+        (err, data) => {
+          if (err) {
+            console.log(err);
+          }
+          console.log(data);
+          // var readStream = new Stream.PassThrough()
+          // readStream.end(data.Body)
+          // readStream.pipe(res)
+          // fs.writeFile('./docs/'+url,data.Body, () => {
+          //   res.download('./docs/'+url,url)
+          // })
+          res.setHeader("Content-disposition", "attachment; filename=" + url);
+          res.setHeader("Content-Type", "application/octet-stream");
+          res.end(data.Body);
+          // res.end(download)
+
+          // res.download(data)
+          // res.end(data.Body);
+        }
+      );
+      // console.log(data)
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    }
+  },
+};
+
+export default pdfController;
--- router/routes.js
+++ router/routes.js
@@ -3,6 +3,7 @@ import pdfController from "../controllers/pdfController.js"
 const router = Router();
 
 router.post('/generatePdf',pdfController.generatePdf)
+router.get('/downloadPdf', pdfController.downloadPdf)
 
 
 
